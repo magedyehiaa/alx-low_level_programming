bf25b887e9564caefa740592995eb05da57cedd7
maged <myeof.ii@gmail.com>
2023-08-11 04:37:12 +0300

    TR1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-	 int main(){
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
-    }
+/**
+  * main - Entry point
+  *
+  * Description: 'main function description printing a text mego'
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
