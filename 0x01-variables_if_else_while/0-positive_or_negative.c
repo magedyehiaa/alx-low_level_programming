a6d1bd5d375da61376b9ef0f24f00448348e9510
maged <myeof.ii@gmail.com>
2023-08-14 02:04:26 +0300

    try3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
   * main - Entry point
   *
-  * Description: 'how to use if else, mego'
+  * Description: 'if else, mego'
   *
   * Return: Always 0 (Success)
   */
@@ -14,10 +14,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%i is postive\n", n);
+printf("%d is positive\n", n);
 else if (n == 0)
-printf("%i is zero\n", n);
+printf("%d is zero\n", n);
 else
-printf("%i is negative", n);
+printf("%c is negative", n);
 return (0);
 }
