a6b65a4c94b2675135a3ae6b070b3ab6e10ae8a4
maged <myeof.ii@gmail.com>
2023-08-14 02:02:16 +0300

    TRY2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is postive\n", n);
+printf("%i is postive\n", n);
 else if (n == 0)
-printf("%d is zero\n", n);
+printf("%i is zero\n", n);
 else
-printf("%d is negative", n);
+printf("%i is negative", n);
 return (0);
 }
