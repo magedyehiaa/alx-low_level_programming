6d360412fcaf9d84f312a73febbc29454b61dbb7
maged <myeof.ii@gmail.com>
2023-08-14 04:00:18 +0300

    TRY0

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'main function description printing a text mego'
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+int maged = 48;
+while (maged <= 102)
+{
+putchar(maged);
+if (maged == 57)
+maged += 39;
+maged++
+}
+putchar('\n');
+return (0);
+}
