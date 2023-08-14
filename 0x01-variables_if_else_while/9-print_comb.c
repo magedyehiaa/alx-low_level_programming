0d0f65dd7a3c117bf28dbf47fdd68f41c29ed3e4
maged <myeof.ii@gmail.com>
2023-08-14 04:13:05 +0300

    TRY0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'reverse l alphabet mego'
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+int m = 0;
+while (m <= 9)
+{
+putchar(m + 48);
+if (m != 9)
+{
+putchar(',');
+putchar(' ');
+}
+m++;
+}
+putchar('\n');
+return (0);
+}
