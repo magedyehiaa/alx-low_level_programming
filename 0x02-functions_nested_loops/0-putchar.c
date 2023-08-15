2e8cf9c7710a8af70469478702f5370c663bdbc1
maged <myeof.ii@gmail.com>
2023-08-16 02:18:18 +0300

    TR1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,11 @@
   * Return: Always 0 (Success)
   */
 int main(void)
+{
 char str[] = "_putchar";
 int M;
 for (M = 0; M < 8; M++)
-_putchar(str[M];
+_putchar(str[M]);
 _putchar('\n');
 return (0);
+}
