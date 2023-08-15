2b0860269571b55011f6760f559a370865b1ac5a
maged <myeof.ii@gmail.com>
2023-08-16 02:39:15 +0300

    TR0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times megoo
+ *
+*/
+void print_alphabet_x10(void)
+{
+int m, a;
+for (a = '0'; m <= 9; a++)
+{
+for (a = 'a'; m <= 'z'; m++)
+	_putchar(ch);
+_putchar('\n')
+}
+}
