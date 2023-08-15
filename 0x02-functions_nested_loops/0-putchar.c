208f1a60c86ec66096ef794facd10a2d8ba35e0f
maged <myeof.ii@gmail.com>
2023-08-16 02:15:11 +0300

    TR0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * main - Entry point
+  *
+  * Description: 'function Dec by mego'
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+char str[] = "_putchar";
+int M;
+for (M = 0; M < 8; M++)
+_putchar(str[M];
+_putchar('\n');
+return (0);
