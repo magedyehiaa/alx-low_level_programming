94f81b278998d33fd9863723e1ffc5c4e98f97d7
maged <myeof.ii@gmail.com>
2023-08-16 02:30:51 +0300

    TR0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - utilizes on _putchar
+ * print alphabet from a to z
+*/
+void print_alphabet(void)
+{
+int M;
+for (M = 'a'; M <= 'z'; M++)
+_putchar(M);
+_putchar('\n');
+return (0);
+}
