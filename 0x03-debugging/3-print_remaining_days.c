fab19af7871efffc195f61a3061237fdb159d990
maged <myeof.ii@gmail.com>
2023-08-18 01:49:38 +0300

    TR0

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+  /**
+   * leap year devided
+   * by 100 and 400 or 4
+   */
 if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month >= 2 && day >= 60)
