e0dd7600a397a14f83fb2f621cdfcb68caec0a60
maged <myeof.ii@gmail.com>
2023-08-19 02:31:32 +0300

    TR0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print 1 to 14 10 times M E G o
+ *
+ * Return: Always 0 (succ)
+*/
+void more_numbers(void)
+{
+int n, r, c;
+for (r = 1; r <= 10; r++)
+{
+for (c = 0; c <= 14; c++)
+{
+n = c;
+if (c > 9)
+{
+_putchar(1 + 48);
+n = c % 10;
+}
+_putchar(n + 48);
+}
+_putchar('n');
+}
