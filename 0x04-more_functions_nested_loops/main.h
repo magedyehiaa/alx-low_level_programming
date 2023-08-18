4163953d2436411778bc1938d270e75f388dfe30
maged <myeof.ii@gmail.com>
2023-08-19 01:53:49 +0300

    TR0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,11 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int m, int a);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif
