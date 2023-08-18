9aab9df9350c250b7c642c2ad03754745a669841
maged <myeof.ii@gmail.com>
2023-08-19 02:49:35 +0300

    TR0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - find new line and print linesM E G O
+ * @n: number of _ printed
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int s;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (s = 1; s <= n; s++)
+_putchar('_');
+_putchar('_')
+}
+}
