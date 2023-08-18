3589afa536c9aae51fdb6e0a852b33e35e374514
maged <myeof.ii@gmail.com>
2023-08-19 00:44:58 +0300

    TR1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers us _putchar 0 to 9, M E G O
+ *
+ * Return: Always 0.
+*/
+void print_numbers(void)
+{
+int num = 0;
+while (num >= 0 && num <= 9)
+{
+_putchar(num + 48);
+N++;
+}
+_putchar('\n');
+}
