c39d52bb8dbc9e2f0d3186b3dea48b551e112275
maged <myeof.ii@gmail.com>
2023-08-29 03:37:11 +0300

    TR2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * *_memset - fil memory with con byte
- * @s: pointer for con
- * @b: con
- * @n: mx byte
- * Return: pointer s
- */
-char *_memset(char *s, char b, unsigned int n);
+* *_memset - fil memory with con byte
+* @s: pointer for con
+* @b: con
+* @n: mx byte
+* Return: pointer s
+*/
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int ed;
 for (ed = 0; n > 0; ed++, n--)
