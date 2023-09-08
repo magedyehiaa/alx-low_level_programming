20bcda7b03146262b352c747f0b8902cacb6a394
maged <myeof.ii@gmail.com>
2023-09-08 20:27:35 +0300

    TR0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - natural square megozos
+ * @n: number
+ * @maged: the root
+ * Return: number0
+ */
+int square(int n, int maged);
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+/**
+ * square - root finding megos
+ * @n: num
+ * @maged: root
+ * Return: number
+ */
+int square(int n, int maged)
+{
+if (maged + maged == n)
+return (maged);
+else if (maged * maged < n)
+return (square(n, maged + 1));
+	else
+	return (-1);
+}
