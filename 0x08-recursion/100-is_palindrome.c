a4bee08962a321d5c0c168658fbb2ff05f5c7564
maged <myeof.ii@gmail.com>
2023-09-08 21:49:56 +0300

    TR0

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * is palindrome - reversed equal M E G oO o z
+ *
+ * @s: the char
+ * Return: 1 if pal 0 if not
+ */
+
+int is_palindrome(char *s);
+{
+char *strrev(char *s);
+if (s == strrev(char *s))
+return (1);
+else
+return (0);
+}
