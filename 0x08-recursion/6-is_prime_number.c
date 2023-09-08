8f55ab02716a46d2a56e46a735a2ffbd17264623
maged <myeof.ii@gmail.com>
2023-09-08 21:35:27 +0300

    TR5

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,12 +6,14 @@
  */
 int is_prime_number(int n)
 {
-  if (n > 0)
-    {
-if (n % n == 0)
+if (n > 0)
+{
+if ( n == 1)
+return (1);
+else if (n % n == 0)
 return (1);
 else
 return (0);
 }
-  return (0);
+return (0);
 	}
