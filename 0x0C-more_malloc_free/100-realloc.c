1d4709284de9c4a522bb2bb70acaf5b5e9a70af9
maged <myeof.ii@gmail.com>
2023-09-08 03:20:16 +0300

    TR5

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *_realloc - allocates a memory block MEgOgOgo.
+ * @ptr: pointer (old_size).
+ * @old_size: size for ptr
+ * @new_size: size in bytes block.
+ *
+ * Return: NULL if new_size = 0 ptr not NULL.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < old_size && i < new_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (p);
+}
