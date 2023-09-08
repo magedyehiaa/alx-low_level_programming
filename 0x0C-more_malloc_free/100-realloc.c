e5063b0aa5017ce19db015fc7f2f6fdf8db11c20
maged <myeof.ii@gmail.com>
2023-09-08 03:17:07 +0300

    TR3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,32 +7,34 @@
  *
  * Return: Null or not null
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *p;
-unsigned int i;
-if (new_size == old_size)
-return (ptr);
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-if (ptr == NULL)
-{
-p = malloc(new_size);
-if (p == NULL)
-return (NULL);
-return (p);
-}
-if (new_size > old_size)
-{
-p = malloc(new_size);
-if (p == NULL)
-return (NULL);
-for (i = 0; i < old_size && i < new_size; i++)
-*((cahr *)p + i) = *((char *)ptr + i);
-free(ptr);
-}
-return (p);
+	void *m;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		m = malloc(new_size);
+		if (m == NULL)
+			return (NULL);
+		return (m);
+	}
+	if (new_size > old_size)
+	{
+		m = malloc(new_size);
+		if (m == NULL)
+			return (NULL);
+		for (i = 0; i < old_size && i < new_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (m);
 }
