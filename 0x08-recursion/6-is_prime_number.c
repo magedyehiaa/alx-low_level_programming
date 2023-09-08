414875cad33280da6be72338fae261d21939ae45
maged <myeof.ii@gmail.com>
2023-09-08 21:38:25 +0300

    TR6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- * is_prime_number - find the prim
+ * is_prime_number - find the prim MeGoO
  * @n: number
  * Return: 1 if prime 0 if other
  */
 int is_prime_number(int n)
 {
+if (n == 1)
+{
+return (1);
+}
 if (n > 0)
 {
-if ( n == 1)
-return (1);
-else if (n % n == 0)
+if (n % n == 0)
 return (1);
 else
 return (0);
 }
 return (0);
-	}
+}
