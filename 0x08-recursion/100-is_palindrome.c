faa7c44121de6ba24d1917b7cde95fa14690f74e
maged <myeof.ii@gmail.com>
2023-09-09 17:48:59 +0300

    TR3

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,42 @@
 #include "main.h"
-bool isPalRec(char str[], int s, int e);
-
 /**
- * isPalRec - if pal
- * @s: term
- * @e: term 2
- * Return: 1 or 0
- * @str: back
+ * last_index - return Me G oOo
+ * @s: pointer
+ * Return: integed
  */
-bool isPalRec(char str[], int s, int e)
+int is_palindrome(char *s);
+int check(char *s, int begain, int ending, int mod);
+int last_index(char *s)
 {
-if (s == e)
-return (1);
-if (str[s] != str[e])
-return (0);
-if (s < e + 1)
-return (isPalRec(str, s + 1, e - 1));
-return (1);
+int i = 0;
+if (*s > '\0')
+i += last_index(s + 1) + 1;
+return (i);
 }
 /**
- * is_palindrome - reversed equal M E G oO o z
- *
- * @s: the char
- *
- * Return: 1 if pal 0 if not
+ * is_palindrome - is it pal MeG oOoz
+ * @s: string
+ * Return: 0 or 1
  */
 int is_palindrome(char *s)
 {
-int n = strlen(str);
-if (n == 0)
+int ending = last_index(s);
+return (check(s, 0, ending - 1, ending % 2));
+}
+/**
+ * check - the checker mego
+ * @s: string
+ * @begain: form r to l
+ * @ending: from l to r
+ * @mod: integer
+ * Return: 0 or 1
+ */
+int check(char *s, int begain, int ending, int mod)
+{
+if ((begain == ending && mod != 0) || (begain == ending + 1 && mod == 0))
 return (1);
-return (isPalRec(str, 0, n - 1));
-return (1);
+else if (s[begain] != s[ending])
+return (0);
+else
+return (check(s, begain + 1, ending - 1, mod));
 }
