cd683c56a2e8aca8b0d721b75710a37540384c20
maged <myeof.ii@gmail.com>
2023-09-09 17:31:51 +0300

    TR1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,35 @@
 #include "main.h"
+bool isPalRec(char str[], int s, int e);
+
 /**
- * is palindrome - reversed equal M E G oO o z
+ * isPalRec - if pal
+ * @s: term
+ * @e: term 2
+ * Return: 1 or 0
+ * @str: back
+ */
+bool isPalRec(char str[], int s, int e)
+{
+if (s == e)
+return (1);
+if (str[s] != str[e])
+return (0);
+if (s < e + 1)
+return (isPalRec(str, s + 1, e - 1));
+return (1);
+}
+/**
+ * is_palindrome - reversed equal M E G oO o z
  *
  * @s: the char
+ *
  * Return: 1 if pal 0 if not
  */
-
-int is_palindrome(char *s);
+int is_palindrome(char *s)
 {
-char *strrev(char *s);
-if (s == strrev(char *s))
+int n = strlen(str);
+if (n == 0)
 return (1);
-else
-return (0);
+return (isPalRec(str, 0, n - 1));
+return (1);
 }
