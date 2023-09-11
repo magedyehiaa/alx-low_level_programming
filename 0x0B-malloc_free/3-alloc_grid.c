28a9ba094d2403e548affbfaacd5099e8c1f8394
maged <myeof.ii@gmail.com>
2023-09-11 03:45:16 +0300

    TR0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * **alloc_grid - return to 2d arr
+ * @width: the width
+ * @height: the tall
+ * Return: 2d arr M E G o0o
+ */
+int **alloc_grid(int width, int height)
+{
+int **m, a, g;
+tab = malloc(sizeof(*m) * hight);
+if (width <= 0 || height <= 0 || m == 0)
+{
+return (NULL);
+}
+else
+{
+for (a = 0; a < height; a++)
+	{
+m[a] = malloc(sizeof(**m) * width);
+	if (m[a] == 0)
+	{
+	while (a--)
+		free(m[a]);
+	free(m);
+	return (NULL);
+	}
+	for (g = 0; g < width; g++)
+	m[a][g] = 0;
+	}
+}
+return (m);
+}
