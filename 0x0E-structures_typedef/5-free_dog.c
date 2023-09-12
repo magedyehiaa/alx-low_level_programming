ecf559efcf1a18b6a5119a8134302a209c0879d2
maged <myeof.ii@gmail.com>
2023-09-12 04:33:30 +0300

    try2

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h"
+
+/**
+ * free_dog - freee the dog megooooooozoozo
+ * @d: same
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+if (d->name)
+free(d->name);
+if (d->owner)
+free(d->owner);
+free(d);
+}
+}
