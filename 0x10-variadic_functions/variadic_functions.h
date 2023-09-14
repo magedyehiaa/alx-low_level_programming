6e83c02bd6fb67571a76eebf16eaf2e12cdd476d
maged <myeof.ii@gmail.com>
2023-09-15 01:38:13 +0300

    try0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,25 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
-
-#include <stdio.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+int _putchar(char c);
+
+/**
+ * struct funckey - key
+ * in 3-print_all.c
+ *
+ * @f: function
+ * @spec: characte
+ */
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
 
 #endif
