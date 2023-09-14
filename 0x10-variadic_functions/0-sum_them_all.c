4f75c54614a309fe98e2a3f2c51987b8cb053443
maged <myeof.ii@gmail.com>
2023-09-15 01:23:18 +0300

    try0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_function.h"
+/**
+ * sum_them_all - sum all of it megooo
+ * @n: co int
+ * @...: int to sum
+ * Return: int sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int m = 0, a = n
+va_list ap;
+if (!n)
+return (0);
+va_start(ap, n);
+while (a--)
+m += va_arg(ap, int);
+va_end(ap);
+return (m);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+/**
+ * print_numbers - printing no
+ * @separator: string
+ * @n: int megoooz
+ * @...: ints to print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+int m = n;
+va_list = ap;
+if (!n)
+{
+printf("\n");
+return;
+}
+va_start(ap, n);
+while (m--)
+printf("%d%s", va_arg(ap, int), m ? (separator ? separator : "") : "\n");
+va_end(ap);
+}
