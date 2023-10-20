419ccf5a91f56e6ec9d2f41fce8a85ee1317b05c
Maged <myeof.ii@gmail.com>
2023-10-20 15:43:51 +0300

    try0 no 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - head node adding
+ * @head: addres of a pointer head
+ * @str: faliur
+ *
+ * Return: size
+ */
+lists_t *add_node(list_t **head, const char *str)
+{
+list_t *nhead = malloc(sizeof(list_t));
+if (!head || !nhead)
+return (NULL);
+if (str)
+	{
+	nhead->str = strdup(str);
+	if (!nhead->str)
+	{
+	free(nhead);
+	return (NULL);
+	}
+	nhead->len = _strlen(nhead->str);
+	}
+nhead->next = *head;
+*head = nhead;
+return (nhead);
+}
