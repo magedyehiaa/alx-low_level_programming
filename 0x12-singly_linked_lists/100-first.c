c4ee13707f66e288f24fd2fbab5a14691b28a58d
Maged <myeof.ii@gmail.com>
2023-10-20 16:05:52 +0300

    try0 no5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - before main code mego
+ *
+ * Return: void
+ */
+void _constructor(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
