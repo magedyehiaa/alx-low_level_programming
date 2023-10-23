43b369d2d666dead80608c8437a914b0cc9187cc
Maged <myeof.ii@gmail.com>
2023-10-23 21:38:02 +0300

    try0 no 4

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new = malloc(sizeof(listint_t));
-listint_ *no;
+listint_t *no;
 if (!head || !new)
 return (NULL);
 new->next = NULL;
