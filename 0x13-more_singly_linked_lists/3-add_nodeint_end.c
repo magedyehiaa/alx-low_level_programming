b47ca80671f0a8d8365f31019c60de7f58a1353c
Maged <myeof.ii@gmail.com>
2023-10-23 21:45:48 +0300

    try0 no 4

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,12 +1,10 @@
 #include "lists.h"
-
 /**
  * add_nodeint_end - cread an ender nod
  * @head: the header node
  * @n: new node value
  * Return: new node
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new = malloc(sizeof(listint_t));
