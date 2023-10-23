3c93021d52b4927cec09626e6d2d5bc5e92933c5
Maged <myeof.ii@gmail.com>
2023-10-23 22:20:30 +0300

    try0 no 4

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - deleting a header node
+ * @head: the header
+ * Return: new list
+ */
+
+int pop_listint(listint_t **h)
+{
+listint_t *nde;
+int m;
+if (!head || !(*head))
+return (0);
+
+nde = (*head)->next;
+m = (*head)->m
+free(*head);
+*head = nde;
+return (m);
+}
