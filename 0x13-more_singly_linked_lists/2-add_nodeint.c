2069467cf54c3e2f2c5ff56b84f26808e3568fdd
Maged <myeof.ii@gmail.com>
2023-10-23 21:27:09 +0300

    try0 no 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - cread a header nod
+ * @head: the header node
+ * @n: new node value
+ * Return: new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new = malloc(sizeof(listint_t));
+if (!head || !node)
+return (NULL);
+new->next = NULL
+new->n = n;
+if (*head)
+new->next = *head;
+*head = new;
+return (new);
+}
