2d1a9f61b00150c9ee329e21f732846fef2c46f2
Maged <myeof.ii@gmail.com>
2023-10-23 20:46:49 +0300

    try2 no 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_listint - printing all elements " Megoo"
+ * @h: first node
+ * Return: no of modes as seen
+ */
 size_t print_listint(const listint_t *h)
 {
 size_t m = 0;
