323747511175ae88d978d7d90a7a0ee34b2e03f1
Maged <myeof.ii@gmail.com>
2023-10-24 22:44:09 +0300

    try0 no 3

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - inserts nodes in a position gvn
+ * @index: deleted index
+ * @head: header node
+ *
+ * Return: 1 or -1.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *node_0, *node_1 = malloc(sizeof(listint_t));
+unsigned int m = 0;
+if (!head || !node_1)
+return (-1);
+
+if (!index)
+{
+node_0 = *head;
+*head = (*head)->next;
+free(node_0);
+return (1);
+}
+node_0 = *head;
+while (node_0)
+{
+if (m == index)
+	{
+	node_1->next = node_0->next;
+	free(node_0);
+	return (1);
+	}
+m++;
+node_1 = node_0;
+node_0 = node_0->next;
+}
+return (-1);
+}
