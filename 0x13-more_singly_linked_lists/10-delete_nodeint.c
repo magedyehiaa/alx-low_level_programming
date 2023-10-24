4257652a42c2bc52fe469d609ae3be2d458c2284
Maged <myeof.ii@gmail.com>
2023-10-24 22:54:19 +0300

    try0 no 3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,14 +7,12 @@
  *
  * Return: 1 or -1.
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *node_0, *node_1 = malloc(sizeof(listint_t));
 unsigned int m = 0;
 if (!head || !node_1)
 return (-1);
-
 if (!index)
 {
 node_0 = *head;
@@ -26,11 +24,11 @@ node_0 = *head;
 while (node_0)
 {
 if (m == index)
-	{
-	node_1->next = node_0->next;
-	free(node_0);
-	return (1);
-	}
+{
+node_1->next = node_0->next;
+free(node_0);
+return (1);
+}
 m++;
 node_1 = node_0;
 node_0 = node_0->next;
