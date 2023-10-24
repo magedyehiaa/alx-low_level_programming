752ac6bafc25e93a844c33f9b613c7f58de1c476
Maged <myeof.ii@gmail.com>
2023-10-24 22:03:27 +0300

    try0 no 3

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - index node retirning
+ *
+ * @head: header
+ * @index: index
+ * Return: new node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *new;
+unsigned int m;
+for (new = head, m = 0; new < index; new = new->next, m++)
+;
+return (new);
+}
