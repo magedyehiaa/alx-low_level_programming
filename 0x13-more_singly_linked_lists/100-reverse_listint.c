46a1e651ca61afd0b6effa89d32bd683e22d30a8
Maged <myeof.ii@gmail.com>
2023-10-24 23:07:47 +0300

    try0 no 3

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - revering function
+ * @head: header nodes
+ *
+ * Return: head place
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *node_0 = NULL, *next = Null;
+if (!head || !*head)
+return (NULL);
+node_0 = *head;
+*head = NULL;
+while (node_0)
+{
+next = node_0->next;
+node_0->next = *head;
+*head = node_0;
+node_0 = next;
+}
+return (*head);
+}
