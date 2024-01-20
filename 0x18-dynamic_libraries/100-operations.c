0b93303807fe0ae70a7ce83a910a91dae844d8a7
magedyehiaa <myeof.ii@gmail.com>
2024-01-20 13:40:15 +0200

    log0

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,25 +1,39 @@
-int add(int a, int b) {
-	return a +b;
+/**
+ * add - adding
+ * sub - subbstituation
+ * mul - multip;ica
+ * dev - deviation
+ * mod - mod
+ */
+int add(int a, int b)
+{
+return (a + b);
 }
 
-int sub(int a, int b) {
-	return a - b;
+int sub(int a, int b)
+{
+return (a - b);
 }
-int mul(int a, int b) {
-	return a * b;
+int mul(int a, int b)
+{
+return (a * b);
 }
-int dev(int a, int b) {
-	if (b == 0) {
-		printf("Error: Division by zero\n");
-		return 0;
+int dev(int a, int b)
+{
+	if (b == 0)
+{
+printf("Error: Division by zero\n");
+return (0);
 	}
-	return a / b;
+	return (a / b);
 }
-int mod( int a, int b) {
-	if (b == 0) {
-		printf("Error: Division by zero\n");
-		return 0;
+int mod( int a, int b)
+{
+	if (b == 0)
+{
+	printf("Error: Division by zero\n");
+	return (0);
 	}
-	return a % b;
+	return (a % b);
 }
 
